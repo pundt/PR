026d53c0805c8dd2ca10075ea92ef8710ccd408b
bbrzWienItcc <franz.pilgerstorfer@bbrz.at>
2015-08-13 09:18:22 +0200

    initial commit

---
+++ TestSuite/TestSuite/BeispielKlasse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace TestSuite
+{
+    /// <summary>
+    /// Diese Klasse kann als Basis für Beispiele verwendet werden
+    /// Sie bietet grundlegende Funktionen an, die von den Prüfungs-Methoden
+    /// in der Programmieren-Klasse zwingend benötigt werden.
+    /// </summary>
+    public class BeispielKlasse
+    {
+        #region ÜberprüfungsMethoden
+        /// <summary>
+        /// Liest den Wert für das angegebene Feld aus
+        /// </summary>
+        /// <param name="feldName">das Feld dessen Wert ausgelesen werden soll</param>
+        /// <returns>der Wert des angegebenen  Feldes</returns>
+        public object WertAuslesen(string feldName)
+        {
+            Type t = this.GetType();
+            FieldInfo fi = t.GetField(feldName, BindingFlags.NonPublic | BindingFlags.Instance);
+            return fi.GetValue(this);
+        }
+        #endregion
+    }
+}
---
+++ TestSuite/TestSuite/Programmieren.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
+
+namespace TestSuite
+{
+    public class Programmieren
+    {
+        /// <summary>
+        /// Prüft ob der Typ ein privates, Instanz-Feld mit dem angegebenen Feld-Namen und Feld-Typ besitzt.
+        /// Die Prüfung wird mittels Assertions-Tests durchgeführt.
+        /// </summary>
+        /// <param name="typ">der Typ der das Feld beinhalten soll</param>
+        /// <param name="feldName">der Name des Feldes das gesucht werden soll</param>
+        /// <param name="feldTyp">der Typ den das Feld haben soll</param>
+        public static void PrüfeFeld(Type typ, string feldName, Type feldTyp)
+        {
+            FieldInfo fieldInfo = typ.GetField(feldName, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            Assert.IsTrue(fieldInfo != null, string.Format("Die Variable \"{0}\" fehlt!", feldName));
+            Assert.IsTrue(fieldInfo.FieldType == feldTyp, string.Format("Die Variable \"{0}\" muss den Datentyp \"{1}\" haben", feldName, feldTyp));
+        }
+        /// <summary>
+        /// Prüft ob ein Feld in einem Typ ein Predicate erfüllt.
+        /// Die Prüfung wird mittels Assertions-Tests durchgeführt.
+        /// </summary>
+        /// <typeparam name="T">Der Typ des zu prüfenden Feldes</typeparam>
+        /// <param name="typ">Der Typ der das Feld beinhalten soll</param>
+        /// <param name="feldName">Der Name des Feldes</param>
+        /// <param name="predicate">Die Routine die den Feld-Wert prüfen soll</param>
+        public static void PrüfeWert<T>(Type typ, string feldName, Predicate<T> predicate)
+        {
+            object o = typ.GetConstructor(new Type[] { }).Invoke(new object[] { });
+
+            MethodInfo miInitialisieren = typ.GetMethod("Initialisieren");
+            if (miInitialisieren != null)
+                miInitialisieren.Invoke(o, null);
+
+            T value = (T)typ.GetMethod("WertAuslesen").Invoke(o, new object[] { feldName });
+
+            Assert.IsTrue(predicate.Invoke(value), string.Format("Der Wert für die Variable \"{0}\" ist ungültig", feldName));
+        }
+    }
+}
