865491fc7db7641b578fa694183c1a5bc4fdab76
bbrzWienItcc <franz.pilgerstorfer@bbrz.at>
2015-08-13 09:29:25 +0200

    rewrote examples, added testsuite, rewrote unitTests

--- Variablen/UT1/UT_Grundlagen.cs
+++ Variablen/UT1/UT_Grundlagen.cs
@@ -2,6 +2,7 @@
 using BSP1;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Reflection;
+using TestSuite;
 
 namespace UT1
 {
@@ -11,21 +12,57 @@ namespace UT1
         [TestMethod]
         public void Aufgabe_1()
         {
-            Person p = new Person();
-            Type t = p.GetType();
-            FieldInfo fiAlter = t.GetField("alter", BindingFlags.NonPublic | BindingFlags.Instance);
-
-            Assert.IsTrue(fiAlter != null, "Die Variable \"alter\" fehlt!");
-            Assert.IsTrue(fiAlter.FieldType == typeof(int), "Die Variable \"alter\" muss den Datentyp \"Integer\" haben");
+            Programmieren.PrüfeFeld(typeof(Person), "alter", typeof(int));
         }
 
         [TestMethod]
         public void Aufgabe_2()
         {
-            Person p = new Person();
-            p.Initialisieren();
-
-            Assert.IsTrue((int) p.AlterAuslesen() > 0 && (int) p.AlterAuslesen() < 150, "Der Wert für die Variable \"alter\" ist ungültig");
+            Programmieren.PrüfeWert<int>(typeof(Person), "alter", (x => { return x > 0 && x < 150; }));
         }
+
+        [TestMethod]
+        public void Aufgabe_3()
+        {
+            Programmieren.PrüfeFeld(typeof(Konto), "kontoStand", typeof(double));
+        }
+
+        [TestMethod]
+        public void Aufgabe_4()
+        {
+            Programmieren.PrüfeWert<double>(typeof(Konto), "kontoStand", (x => { return x > -100000 && x < 999999999; }));
+        }
+
+        [TestMethod]
+        public void Aufgabe_5()
+        {
+            Programmieren.PrüfeFeld(typeof(Auto), "ps", typeof(int));
+            Programmieren.PrüfeFeld(typeof(Auto), "kennzeichen", typeof(string));
+        }
+
+        [TestMethod]
+        public void Aufgabe_6()
+        {
+            Programmieren.PrüfeWert<string>(typeof(Auto), "kennzeichen", (x => string.IsNullOrEmpty(x)));
+            Programmieren.PrüfeWert<string>(typeof(Auto), "kennzeichen", (x => x.StartsWith("W")));
+            Programmieren.PrüfeWert<int>(typeof(Auto), "ps", (x => x >= 50 && x <= 300));
+        }
+
+        [TestMethod]
+        public void Aufgabe_7()
+        {
+            Programmieren.PrüfeFeld(typeof(Haus), "höhe", typeof(int));
+            Programmieren.PrüfeFeld(typeof(Haus), "länge", typeof(int));
+            Programmieren.PrüfeFeld(typeof(Haus), "breite", typeof(int));
+        }
+
+        [TestMethod]
+        public void Aufgabe_8()
+        {
+            Programmieren.PrüfeWert<int>(typeof(Haus), "höhe", (x => x >= 1 && x <= 100));
+            Programmieren.PrüfeWert<int>(typeof(Haus), "länge", (x => x >= 1 && x <= 100));
+            Programmieren.PrüfeWert<int>(typeof(Haus), "breite", (x => x >= 1 && x <= 100));
+        }
+
     }
 }
