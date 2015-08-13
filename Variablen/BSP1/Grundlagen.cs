d093bdc4937b9e9b250e35890a2b72344c5f35fb
bbrzWienItcc <franz.pilgerstorfer@bbrz.at>
2015-08-13 09:32:23 +0200

    rewrote examples, added testsuite, rewrote unitTests

---
+++ Variablen/BSP1/Grundlagen.cs
@@ -0,0 +1,63 @@
+﻿using TestSuite;
+
+namespace BSP1
+{
+    /// Zur Überprüfung ob Sie ein Beispiel richtig gemacht haben gehen Sie wie folgt vor:
+    /// 1. Öffnen Sie den entsprechenden Test (also für BSP1 -> UT1) 
+    /// 2. Öffnen Sie darin die entsprechende Test-Datei (zB: UT_Grundlagen.cs)
+    /// 3. Ausführen von Tests
+    ///     3.1 alle Tests ausführen - rechte Maustaste in die Datei - "Tests ausführen"
+    ///     3.2 einen bestimmten Test ausführen - im Test-Explorer einen Test auswählen
+    ///         rechte Maustause auf den ausgewählten Test - "Ausgewählte Tests ausführen"
+
+    /// Sie sehen das Ergebnis der Testfälle im Test-Explorer
+    /// Sie können diesen jederzeit wieder über die 
+    ///         Menüleiste - Test - Fenster - Text-Explorer öffnen
+
+    public class Person : BeispielKlasse
+    {
+        /// TODO - Aufgabe 1: Legen Sie eine int Variable alter an
+        
+
+        public void Initialisieren()
+        {
+            /// TODO - Aufgabe 2: Weisen Sie der Variable alter Ihr Alter zu!
+
+        }
+    }
+
+    public class Konto : BeispielKlasse
+    {
+        /// TODO - Aufgabe 3: Legen Sie eine double Variable kontoStand an
+
+        public void Initialisieren()
+        {
+            /// TODO - Aufgabe 4: Weisen Sie der Variable kontoStand Ihren gewünschten Kontostand zu!
+
+        }
+    }
+
+    public class Auto : BeispielKlasse
+    {
+        /// TODO - Aufgabe 5: Legen Sie eine string Variable kennzeichen und
+        /// eine int Variable ps an
+
+        public void Initialisieren()
+        {
+            /// TODO - Aufgabe 6: Weisen Sie kennzeichen ein Wiener Kennzeichen zu (beginnend mit W)
+            /// Weisen Sie ps einen Wert zwischen 50 und 300 zu
+
+        }
+    }
+
+    public class Haus : BeispielKlasse
+    {
+        /// TODO - Aufgabe 7: Legen Sie drei int Variable höhe, länge und breite an
+
+        public void Initialisieren()
+        {
+            /// TODO - Aufgabe 8: Weisen Sie jeder dieser Variablen einen Wert zwischen 1 und 100 zu
+
+        }
+    }
+}
